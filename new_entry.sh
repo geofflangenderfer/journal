bd256751586debdaefabb512b26b941b6417c6ec
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2020-01-16 08:34:19 -0600

    making progress

--- new_entry.sh
+++ new_entry.sh
@@ -2,20 +2,25 @@
 
 JOURNAL=$HOME/work/journal/
 main() {
-  date=get_date()
-  file_name=create_file_name($date)
-  create_journal_entry(file_name)
-  loc=get_loc_trip_scraper()
-  echo "$loc" >> $JOURNAL/$file_name
+  #date=$($get_date())
+  #echo $date
+  #file_name=create_file_name($date)
+  #create_journal_entry(file_name)
+  loc=${get_loc_trip_scraper()[@]}
+  echo $loc #"$loc" >> $JOURNAL/$file_name
 }
 
 get_date() {
+  return $now
 }
 create_file_name() {
 }
 create_journal_entry() {
 }
 get_loc_trip_scraper() {
+  cd ~/work/gigMax
+  LOC=($(git ls-files | xargs wc -l))
+  return LOC
 }
 
 main
