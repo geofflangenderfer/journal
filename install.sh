87e162c673594d6ff0339d88de908a8ed87d735c
geoff langenderfer <geoff.langenderfer@gmail.com>
2021-05-01 14:20:36 -0500

    created new repo for private journal

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # this may be different on mac/windows
 local_executable_directory=/usr/local/bin
+desired_journal_location=~/work/private_journal/journal.md
 
 sudo cp journal $local_executable_directory/journal
-touch journal.md
+touch $desired_journal_location
