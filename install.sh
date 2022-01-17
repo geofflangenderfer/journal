89692defc11cfea3e7b296bc7cc51d84fa3a474d
Geoff Langenderfer <lgeoff@amazon.com>
2022-01-16 21:35:12 -0800

    add 75 hard goals

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 # this may be different on mac/windows
-local_executable_directory=/usr/local/bin
+local_executable_directory=~/.bin
 desired_journal_location=~/work/private_journal/journal.md
 
+mkdir $local_executable_directory
 sudo cp journal $local_executable_directory/journal
 touch $desired_journal_location
+
+
