1e2ea583f37127c0f9bbc0177eefae51747de303
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2025-05-10 15:52:32 -0700

    fix: ~ is not aut-expanded when used in .zshrc

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ done
 
 cp journal $local_executable_directory/journal
 shell_name="$(basename $(echo $SHELL))"
-echo 'export PATH="~/.local/bin/:$PATH"' >> ~/."$shell_name"rc
+echo 'export PATH="$HOME/.local/bin/:$PATH"' >> ~/."$shell_name"rc
 touch $(echo $desired_journal_directory)/journal.md
 
 
